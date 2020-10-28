ae605fd6cbd9d13186e9817f769dff64c7ecac45
Urhengulas <johann.hemmann@code.berlin>
2020-10-28 23:14:36 +0100

    pltut: Add skeleton for InputStream

--- pltut/src/main.rs
+++ pltut/src/main.rs
@@ -1,3 +1,34 @@
+#[derive(Clone, Debug)]
+struct InputStream {
+    input: Vec<char>,
+    pos: usize,
+    line: usize,
+    col: usize,
+}
+
+impl InputStream {
+    pub fn new(s: &str) -> Self {
+        todo!()
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        todo!()
+    }
+
+    pub fn next(&mut self) -> Option<char> {
+        todo!()
+    }
+
+    pub fn eof(&self) -> bool {
+        todo!()
+    }
+
+    pub fn croak(&self, err_msg: &str) {
+        todo!()
+    }
+}
+
 fn main() {
-    println!("Hello, world")
+    let input = InputStream::new("let a = 1 + 2");
+    dbg!(input);
 }
