ff94dcd61013f27ed4ab9199f56e9ceba3d2f5d0
Urhengulas <johann.hemmann@code.berlin>
2020-10-28 22:17:53 +0100

    eldiro: Evaluate bindings (part 5)

--- eldiro/src/env.rs
+++ eldiro/src/env.rs
@@ -11,4 +11,11 @@ impl Env {
     pub fn store_binding(&mut self, name: String, val: Val) {
         self.bindings.insert(name, val);
     }
+
+    pub fn get_binding_value(&self, name: &str) -> Result<Val, String> {
+        self.bindings
+            .get(name)
+            .cloned()
+            .ok_or_else(|| format!("Binding with name '{}' does not exist", name))
+    }
 }
--- eldiro/src/val.rs
+++ eldiro/src/val.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Val {
     Number(i32),
 }
