1f1c4533a915aba919c9292b641021e058603d38
Urhengulas <johann.hemmann@code.berlin>
2020-10-28 15:14:16 +0100

    eldiro: Parse number

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+#[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        Self(s.parse().unwrap())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parse_number() {
+        assert_eq!(Number::new("123"), Number(123));
     }
 }
