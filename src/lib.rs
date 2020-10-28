33a53139c03bee9eb3807fe01a4ce376a6c9710b
Urhengulas <johann.hemmann@code.berlin>
2020-10-28 15:43:58 +0100

    eldiro: Parse operators

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,26 @@ impl Number {
     }
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    pub fn new(s: &str) -> Self {
+        match s {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Mul,
+            "/" => Self::Div,
+            _ => panic!("Couldn't parse operator"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,5 +34,25 @@ mod tests {
     #[test]
     fn parse_number() {
         assert_eq!(Number::new("123"), Number(123));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), Op::Add);
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), Op::Sub);
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), Op::Mul);
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), Op::Div);
     }
 }
